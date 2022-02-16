77cc7c7e9fb83c4636b01c3398453779d2661519
oswin-jerome <oswinjeromej@gmail.com>
2022-02-16 22:41:41 +0530

    Meta tags

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -18,13 +18,13 @@ class MyDocument extends Document {
 <meta property="og:url" content="https://oswinjerome.in/"/>
 <meta property="og:title" content="Oswin Jerome"/>
 <meta property="og:description" content="Hey I'm Oswin Jerome, I have 5 years of experience in programing. I'm specialized in Websites, Web Apps, Android Apps and lot more. I'm always eager to learn new technalogies and I have a deep knowledge in latest technalogies and frameworks"/>
-<meta property="og:image" content="/logo512.png"/>
+<meta property="og:image" content="/oswin.png"/>
 
 <meta property="twitter:card" content="summary_large_image"/>
 <meta property="twitter:url" content="https://oswinjerome.in/"/>
 <meta property="twitter:title" content="Oswin Jerome"/>
 <meta property="twitter:description" content="Hey I'm Oswin Jerome, I have 5 years of experience in programing. I'm specialized in Websites, Web Apps, Android Apps and lot more. I'm always eager to learn new technalogies and I have a deep knowledge in latest technalogies and frameworks"/>
-<meta property="twitter:image" content="/logo512.png"/>
+<meta property="twitter:image" content="/oswin.png"/>
         </Head>
         <body>
             <NavBar/>
