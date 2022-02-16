7629f29dffba2002815fd1baf41427561e84168b
oswin-jerome <oswinjeromej@gmail.com>
2022-02-16 22:07:35 +0530

    My works & about me

--- components/recentWorks.tsx
+++ components/recentWorks.tsx
@@ -8,7 +8,7 @@ const RecentWorks = () => {
             <p className='absolute text-[12vw] lg:text-[8vw] font-extrabold top-10 lg:top-0 -z-10 text-accent opacity-20'>My Recent Works</p>
             <h1 className='text-2xl lg:text-5xl font-bold text-gray-700 z-10 -mt-6 lg:mt-0 lg:w-2/4'>
             Here are a few design projects 
-I've worked on recently.
+I have worked on recently.
                 </h1>
 
                 <div className='mt-8 lg:mt-16 mb-12 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-5 gap-5'>
