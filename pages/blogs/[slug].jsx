ea3560ae47d9b2cb04baa01c33a7eee12d766de3
oswin-jerome <oswinjeromej@gmail.com>
2022-03-10 12:39:34 +0530

    SEO tags

--- pages/blogs/[slug].jsx
+++ pages/blogs/[slug].jsx
@@ -3,11 +3,26 @@ import { Client } from "@notionhq/client";
 import Head from 'next/head';
 import React from "react";
 import { NotionRenderer } from "react-notion";
-const BlogDetails = ({blog,title}) => {
+const BlogDetails = ({blog,title,cover}) => {
   return (
     <div className="container mx-auto relative pt-12 dark:text-white min-h-[45vh] px-4 lg:px-0 mb-24 lg:mb-60 ">
       <Head>
         <title>{title}</title>
+<title>Tools I use on my laptop</title>
+<meta name="title" content={title}/>
+<meta name="description" content="These are the blog descriptions"/>
+
+<meta property="og:type" content="website"/>
+{/* <meta property="og:url" content="https://www.oswinjerome.in/blogs/tools-I-use"/> */}
+<meta property="og:title" content={title}/>
+<meta property="og:description" content="These are the blog descriptions"/>
+<meta property="og:image" content={cover}/>
+
+<meta property="twitter:card" content="summary_large_image"/>
+{/* <meta property="twitter:url" content="https://www.oswinjerome.in/blogs/tools-I-use"/> */}
+<meta property="twitter:title" content={title}/>
+<meta property="twitter:description" content="These are the blog descriptions"/>
+<meta property="twitter:image" content={cover}></meta>
       </Head>
       <div className='prose dark:text-white/60 prose-strong:text-white/60 prose-headings:text-accent prose-h1:text-accent' dangerouslySetInnerHTML={{__html: blog}}></div>
       {/* <p> {blog}</p> */}
@@ -58,6 +73,7 @@ export const getStaticProps = async ({ params }) => {
     props: {
       blog:html,
       title:title,
+      cover:cover,
     },
   };
 };
