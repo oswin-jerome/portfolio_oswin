f6499620ad80ca3121c517fee36a6019cbe2d494
oswin-jerome <oswinjeromej@gmail.com>
2022-11-13 10:21:32 +0530

    favicon

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -10,7 +10,15 @@ class MyDocument extends Document {
   render() {
     return (
       <Html className="bg-light-accentLight dark:bg-dark-background ">
-        <Head />
+        <Head>
+          <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+          <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+          <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+          <link rel="manifest" href="/site.webmanifest" />
+          <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#5bbad5" />
+          <meta name="msapplication-TileColor" content="#da532c" />
+          <meta name="theme-color" content="#ffffff"></meta>
+        </Head>
         <body>
           {/* <NavBar/> */}
           <Main />
