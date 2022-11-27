4cb85bc9046fb23620407c3f5c9ad7e95d57509e
oswin-jerome <oswinjeromej@gmail.com>
2022-11-27 14:24:45 +0530

    sitemap fix

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,6 +1,6 @@
 /** @type {import('next-sitemap').IConfig} */
 module.exports = {
-  siteUrl: process.env.SITE_URL || "https://oswinjerome.in",
+  siteUrl: process.env.SITE_URL || "https://www.oswinjerome.in",
   generateRobotsTxt: true, // (optional)
   generateIndexSitemap: false,
   // ...other options
