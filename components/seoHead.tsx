7dff4f024efce8d837a90aef8b0e5eba56196615
oswin-jerome <oswinjeromej@gmail.com>
2022-11-27 14:38:46 +0530

    seo fix

--- components/seoHead.tsx
+++ components/seoHead.tsx
@@ -1,32 +1,27 @@
+import Head from "next/head";
+import React from "react";
 
-import Head from 'next/head';
-import React from 'react';
+const SeoHead = ({ title, description, image, url }: any) => {
+  return (
+    <Head>
+      <title>{title}</title>
+      <meta name="title" content={title} />
+      <meta name="description" content={description} />
 
-const SeoHead = ({
-    title,
-    description,
-    image,
-    url
-}:any)=>{
-    return (
-        <Head>
-            <title>{title}</title>
-            <meta name="title" content={title}/>
-            <meta name="description" content={description}/>
-            
-            <meta property="og:type" content="website"/>
-            <meta property="og:url" content={url}/>
-            <meta property="og:title" content={title}/>
-            <meta property="og:description" content={description}/>
-            <meta property="og:image" content={image}/>
-            
-            <meta property="twitter:card" content="summary_large_image"/>
-            <meta property="twitter:url" content={url}/>
-            <meta property="twitter:title" content={title}/>
-            <meta property="twitter:description" content={description}/>
-            <meta property="twitter:image" content={image}/>
-        </Head>
-    );
-}
+      <meta property="og:type" content="website" />
+      <meta property="og:url" content={url} />
+      <meta property="og:title" content={title} />
+      <meta property="og:description" content={description} />
+      <meta property="og:image" content={process.env.APP_URL + image} />
+      <meta property="og:image:url" content={image} />
+
+      <meta property="twitter:card" content="summary_large_image" />
+      <meta property="twitter:url" content={url} />
+      <meta property="twitter:title" content={title} />
+      <meta property="twitter:description" content={description} />
+      <meta property="twitter:image" content={process.env.APP_URL + image} />
+    </Head>
+  );
+};
 
 export default SeoHead;
