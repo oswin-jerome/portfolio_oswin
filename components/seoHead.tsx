a5b161ec1b01496fddcbed8114ae869772a70d40
oswin-jerome <oswinjeromej@gmail.com>
2022-11-27 15:26:09 +0530

    new imagr

--- components/seoHead.tsx
+++ components/seoHead.tsx
@@ -13,7 +13,7 @@ const SeoHead = ({ title, description, image, url }: any) => {
       <meta property="og:title" content={title} />
       <meta property="og:description" content={description} />
       <meta property="og:image" content={process.env.APP_URL + image} />
-      <meta property="og:image:url" content={process.env.APP_URL ?? "localhost:3000" + image} />
+      {/* <meta property="og:image:url" content={process.env.APP_URL ?? "localhost:3000" + image} /> */}
 
       <meta property="twitter:card" content="summary_large_image" />
       <meta property="twitter:url" content={url} />
