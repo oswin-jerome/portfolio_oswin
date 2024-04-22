fee6a14cd7fc6aa2b7ca4c431a981f59b941f1ad
oswin-jerome <oswinjeromej@gmail.com>
2024-04-22 14:31:14 +0530

    SEO

--- src/app/projects/[slug]/page.tsx
+++ src/app/projects/[slug]/page.tsx
@@ -3,6 +3,7 @@ import fs from "fs";
 import path from "path";
 import matter from "gray-matter";
 import { Marked } from "@ts-stack/markdown";
+import { Metadata, ResolvingMetadata } from "next";
 
 const getData = (slug: string) => {
   const markdownWithMeta = fs.readFileSync(path.join(path.resolve(__dirname.split(".next")[0]), "/md/works", slug + ".md"), "utf-8");
@@ -37,15 +38,41 @@ export async function generateStaticParams() {
   }));
 }
 
+type Props = {
+  params: { slug: string };
+  searchParams: { [key: string]: string | string[] | undefined };
+};
+
+export async function generateMetadata({ params, searchParams }: Props, parent: ResolvingMetadata): Promise<Metadata> {
+  // read route params
+  const slug = params.slug;
+
+  // fetch data
+  const { frontmatter } = await getData(slug);
+
+  // optionally access and extend (rather than replace) parent metadata
+  const previousImages = (await parent).openGraph?.images || [];
+
+  return {
+    title: frontmatter.title,
+    description: frontmatter.excerpt,
+    openGraph: {
+      images: [frontmatter.cover_image, ...previousImages],
+    },
+  };
+}
+
 const ProjectDetailsPage = ({ params: { slug } }: { params: { slug: string } }) => {
   const {
     frontmatter: { title, date, cover_image, excerpt },
     // slug,
     content,
   } = getData(slug);
+
   return (
     <div>
       <SeoHead image={cover_image} title={"Project - " + title} description={excerpt} url={`https://oswinjerome.in/projects/${slug}`} />
+
       <div className="post container mx-auto mt-16 p-8">
         {/* <h1 className='text-4xl font-bold mb-5'>{title}</h1> */}
         <div className="markdown prose prose-a:bg-accent prose-a:p-3 prose-a:rounded prose-a:shadow prose-a:no-underline dark:text-white prose-headings:text-accent prose-h1:text-accent dark:prose-a:text-blue-400 max-w-full" dangerouslySetInnerHTML={{ __html: Marked.parse(content) }}></div>
